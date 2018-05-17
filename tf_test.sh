baf420b3a9ee3e99aa080929a4553bdd0c259b5f
yuma <y.kishi@m.cnl.t.u-tokyo.ac.jp>
2018-05-17 10:50:45 +0900

    fix script

--- tf_test.sh
+++ tf_test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#PBS -q h-regular 
-#PBS -l select=8:mpiprocs=1:ompthreads=1 
+#PBS -q h-regular
+#PBS -l select=1:mpiprocs=1:ompthreads=1
 #PBS -W group_list=gj16
-#PBS -l walltime=02:00:00 
-cd $PBS_O_WORKDIR 
-. /etc/profile.d/modules.sh 
-module load tensorflow 
+#PBS -l walltime=20:00:00 
+cd $PBS_O_WORKDIR
+. /etc/profile.d/modules.sh
+module load tensorflow
 python main.py --dataset celebA --input_height=108 --train --crop
